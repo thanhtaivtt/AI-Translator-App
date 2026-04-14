90155f086d5bd6034e210e75edc08a1aad29d3d5
thanhtaivtt <thanhtai96nd@gmail.com>
2026-04-14 19:25:06 +0700

    Initial Commit

---
+++ AITranslator/AITranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITranslatorApp.swift
+//  AITranslator
+//
+//  Created by TaiVT on 14/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AITranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AITranslatorTests/AITranslatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AITranslatorTests.swift
+//  AITranslatorTests
+//
+//  Created by TaiVT on 14/4/26.
+//
+
+import Testing
+@testable import AITranslator
+
+struct AITranslatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
